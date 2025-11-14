72956dd417a0afd6b1b5011bfe3ca1df312d0d20
archana <archanarackenchath@gmail.com>
2025-11-14 09:33:17 +0530

    astro path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://subbuandco.com/',   // <-- IMPORTANT
-  base: '/',                         // root domain
-  trailingSlash: 'always',           // GitHub Pages fix
-  outDir: './dist',                  // default
+  site: 'https://subbuandco.com/',
+  base: '/',
+  trailingSlash: 'always',
+  build: {
+    assets: '_astro',   // required for correct asset folder
+  },
 });
